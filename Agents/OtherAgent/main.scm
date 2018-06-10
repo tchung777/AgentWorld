f4a696d9775510a3bfbe5e2a9f8a874d242dfce2
VersaceSauce <chchung@tesla.com>
2018-06-09 22:58:56 -0700

    Updated OtherAgent.

--- Agents/OtherAgent/main.scm
+++ Agents/OtherAgent/main.scm
@@ -17,21 +17,7 @@
 ; 7, 9, and 11, representing the contents of spaces up to 5 positions away from
 ; the agent in the direction it is facing.
 (define (choose-action current-energy previous-events percepts)
-    (let ((rand (random 12)))
-        ; (cond ((equal? rand 0) "STAY")
-        ;     ((equal? rand 1) "TURN-RIGHT")
-        ;     ((equal? rand 2) "TURN-LEFT")
-        ;     ((equal? rand 3) "TURN-AROUND")
-        ;     ((equal? rand 4) "MOVE-PASSIVE-1")
-        ;     ((equal? rand 5) "MOVE-PASSIVE-2")
-        ;     ((equal? rand 6) "MOVE-PASSIVE-3")
-        ;     ((equal? rand 7) "MOVE-AGGRESSIVE-1")
-        ;     ((equal? rand 8) "MOVE-AGGRESSIVE-2")
-        ;     ((equal? rand 9) "MOVE-AGGRESSIVE-3")
-        ;     ((equal? rand 10) "EAT-PASSIVE")
-        ;     ((equal? rand 11) "EAT-AGGRESSIVE")
-        ;     (#f "STAY"))))
-        (just-run percepts)))
+        (just-run previous-events percepts))
 
 
 ; A few rules to walk around. Eat vegetation when we run into it, and run from
@@ -39,10 +25,11 @@
 ; 
 ; INPUT:
 ; OUTPUT:
-(define (just-run percepts)
+(define (just-run previous-events percepts)
     (let ((rand (random 2)))  ; roll of dice for what we should do
-        (cond 
-            ((list? (cadr (car percepts)))  ; we have something in front of us
+        (cond
+            ; We have something in front of us
+            ((list? (cadr (car percepts)))
                 (cond 
                     ((equal? (car (cadr (car percepts))) 'agent)  ; agent in front of us
                         (cond 
@@ -51,15 +38,29 @@
                     ((equal? (car (cadr (car percepts))) 'predator) "TURN-AROUND")  ; predator in front of us
                     ((equal? (car (cadr (car percepts))) 'vegetation)  ; vegetation in front of us
                         (cond 
-                            ((< (tail (cadr (car percepts))) 10)  ; only eat if it's worth it
+                            ((equal? (tail (cadr (car percepts))) 0)  ; only eat if it's worth it
                                 (cond 
                                     ((equal? rand 0) "TURN-RIGHT")
                                     ((equal? rand 1) "TURN-LEFT")))
                             (#t "EAT-AGGRESSIVE")))))
-            ((equal? (car percepts) '(barrier barrier barrier))  ; move away from barrier
+
+            ; Move away from the barrier
+            ((equal? (car percepts) '(barrier barrier barrier))
                 (cond 
                     ((equal? rand 0) "TURN-RIGHT")
                     ((equal? rand 1) "TURN-LEFT")))
+
+            ; We were just attacked ... RUN
+            ((was-attacked? previous-events)
+                (cond
+                    ((list? (cadr (car percepts)))  ; something in front of us
+                        (cond 
+                            ((equal? rand 0) "TURN-RIGHT")
+                            ((equal? rand 1) "TURN-LEFT")))
+                    ((list? (nth-item 3 (nth-item 2 percepts)))  "MOVE-AGGRESSIVE-1")  ; something 2 away from us
+                    (#t "MOVE-AGGRESSIVE-2")))
+            
+            ; Predator is somewhere in front of us
             ((list? (nth-item 6 (nth-item 5 percepts)))  ; predator 5 away from us
                 (cond 
                     ((equal? (car (nth-item 6 (nth-item 5 percepts))) 'predator) "TURN-RIGHT")
@@ -76,6 +77,8 @@
                 (cond 
                     ((equal? (car (nth-item 3 (nth-item 2 percepts))) 'predator) "TURN-RIGHT")
                     (#t "MOVE-PASSIVE-1")))
+
+            ; Move forward if you can
             ((equal? (cadr (car percepts)) 'empty) "MOVE-PASSIVE-1")  ; move forward if you can
             (#t "TURN-RIGHT"))))
 
@@ -97,3 +100,19 @@
 (define (nth-item index list)
     (cond ((equal? 1 index) (car list))  ; base case, index is equal to 0, return
         (#t (nth-item (- index 1) (cdr list)))))  ; decrement the index and get tail of list
+
+
+; Checks if a single event contains whether or not we were attacked
+; 
+; INPUT: (was-attacked-helper '(attacked-by 1 -50))
+; OUTPUT: #t
+(define (was-attacked-helper single-event)
+    (list? (member 'attacked-by single-event)))
+
+
+; Returns true if we were just previously attacked
+; 
+; INPUT: (was-attacked? '((moved 1) (attacked-by 1 -50)))
+; OUTPUT: #t
+(define (was-attacked? previous-events)
+    (list? (member #t (map was-attacked-helper previous-events))))
